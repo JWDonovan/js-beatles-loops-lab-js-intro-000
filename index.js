cd5d28e81eb943a369a8ff959b774530d6df36fb
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 17:59:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments) {
-  let array = [];
-
-  for (let i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-
-  return array;
-}
-
-function johnLennonFacts(facts) {
-  let array = [];
-
-  for (let i = 0; i < facts.length; i++) {
-    array.push(`${facts[i]!!!}`);
-  }
-
-  return array;
-}
+// function theBeatlesPlay(musicians, instruments) {
+//   let array = [];
+//
+//   for (let i = 0; i < musicians.length; i++) {
+//     array.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+//
+//   return array;
+// }
+//
+// function johnLennonFacts(facts) {
+//   let array = [];
+//
+//   for (let i = 0; i < facts.length; i++) {
+//     array.push(`${facts[i]!!!}`);
+//   }
+//
+//   return array;
+// }
 
 function iLoveTheBeatles(num) {
   let array = [];
